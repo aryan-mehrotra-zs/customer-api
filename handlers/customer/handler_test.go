742946c3736b923628a4e2aabeaf0c1c0a29e4fa
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2022-01-23 15:12:18 +0530

    add check to response body in create test

--- handlers/customer/handler_test.go
+++ handlers/customer/handler_test.go
@@ -22,12 +22,13 @@ func TestHandler_Create(t *testing.T) {
 		resp       []byte
 		statusCode int
 	}{
-		{"entity already exists", bytes.NewReader([]byte(`{"id":4,"name":"Umang"}`)), nil, http.StatusOK},
-		{"create new entity", bytes.NewReader([]byte(`{"name":"Aryan"}`)), nil, http.StatusCreated},
-		{"missing or invalid parameter", bytes.NewReader([]byte(`{"name":"Ruchit"}`)), nil, http.StatusBadRequest},
-		{"default case : internal server error", bytes.NewReader([]byte(`{"name":"Aakanksha"}`)), nil, http.StatusInternalServerError},
-		{"unmarshal error", bytes.NewReader([]byte(`invalid body`)), nil, http.StatusBadRequest},
-		{"unable to read body", mockReader{}, nil, http.StatusInternalServerError},
+		{"entity already exists", bytes.NewReader([]byte(`{"id":4,"name":"Umang"}`)), []byte(""), http.StatusOK},
+		{"create new entity", bytes.NewReader([]byte(`{"id":1,"name":"Aryan","address":"","phone_no":0}`)), []byte(`{"id":1,"name":"Aryan","address":"","phone_no":0}`), http.StatusCreated},
+		{"invalid parameter", bytes.NewReader([]byte(`{"NAME":"Ruchit"}`)), []byte(""), http.StatusBadRequest},
+		{"missing parameter", bytes.NewReader([]byte(`{"name":"Ruchit"}`)), []byte(""), http.StatusBadRequest},
+		{"default case : internal server error", bytes.NewReader([]byte(`{"name":"Aakanksha"}`)), []byte(""), http.StatusInternalServerError},
+		{"unmarshal error", bytes.NewReader([]byte(`invalid body`)), []byte(""), http.StatusBadRequest},
+		{"unable to read body", mockReader{}, []byte(""), http.StatusInternalServerError},
 	}
 
 	for i, tc := range cases {
@@ -38,13 +39,22 @@ func TestHandler_Create(t *testing.T) {
 
 		resp := w.Result()
 
-		err := resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Errorf("cannot read resp: %v", err)
+		}
+
+		err = resp.Body.Close()
 		if err != nil {
 			t.Errorf("error in writing response")
 		}
 
 		if resp.StatusCode != tc.statusCode {
 			t.Errorf("\n[TEST %d] Failed. Desc : %v\nGot %v\nExpected %v", i, tc.desc, resp.StatusCode, tc.statusCode)
+		}
+
+		if !reflect.DeepEqual(body, tc.resp) {
+			t.Errorf("\n[TEST %d] Failed. Desc : %v\nGot %v\nExpected %v", i, tc.desc, string(body), string(tc.resp))
 		}
 	}
 }
@@ -76,6 +86,11 @@ func TestHandler_GetByID(t *testing.T) {
 		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			t.Errorf("cannot read resp: %v", err)
+		}
+
+		err = resp.Body.Close()
+		if err != nil {
+			t.Errorf("error in writing response")
 		}
 
 		if resp.StatusCode != tc.statusCode {
@@ -150,13 +165,18 @@ func TestHandler_DeleteByID(t *testing.T) {
 	}
 
 	for i, tc := range cases {
-		req := httptest.NewRequest(http.MethodDelete, "http://customer", nil)
+		req := httptest.NewRequest(http.MethodDelete, "http://customer", http.NoBody)
 		r := mux.SetURLVars(req, map[string]string{"id": tc.id})
 		w := httptest.NewRecorder()
 
 		h.DeleteByID(w, r)
 
 		resp := w.Result()
+
+		err := resp.Body.Close()
+		if err != nil {
+			t.Errorf("error in writing response")
+		}
 
 		if resp.StatusCode != tc.statusCode {
 			t.Errorf("\n[TEST %d] Failed. Desc : %v\nGot %v\nExpected %v", i, tc.desc, resp.StatusCode, tc.statusCode)
